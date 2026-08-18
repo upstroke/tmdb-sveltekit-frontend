6749dbb0bbf8c932d3664b700df809e14e3f570e
gregorarz <upstroke@webtutorials.de>
2026-08-18 14:49:24 +0200

    refine test structure and add setup for tests, add various tests, add descriptions to readme.md

--- playwright.config.js
+++ playwright.config.js
@@ -1,21 +1,20 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: 'e2e',
-  use: {
-    baseURL: 'http://127.0.0.1:4173',
-    trace: 'on-first-retry'
-  },
-  webServer: {
-    command: 'npm run build && npm run preview -- --host 127.0.0.1 --port 4173',
-    url: 'http://127.0.0.1:4173',
-    reuseExistingServer: !process.env.CI
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
-    }
-  ]
+	testDir: 'tests/acceptance',
+	use: {
+		baseURL: 'http://127.0.0.1:4173',
+		trace: 'on-first-retry'
+	},
+	webServer: {
+		command: 'npm run build && npm run preview -- --host 127.0.0.1 --port 4173',
+		url: 'http://127.0.0.1:4173',
+		reuseExistingServer: !process.env.CI
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		}
+	]
 });
-
