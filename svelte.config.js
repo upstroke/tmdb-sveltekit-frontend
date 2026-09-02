9d1feacccde02b8897bf16ff320061a46e8cc953
Gregor Arz <upstroke@webtutorials.de>
2026-09-02 18:02:09 +0200

    Add kit.alias for $lib and $tests path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,13 @@ import adapter from '@sveltejs/adapter-auto';
 
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'$lib': 'src/lib',
+			'$lib/*': 'src/lib/*',
+			'$tests': 'tests',
+			'$tests/*': 'tests/*'
+		}
 	},
 	compilerOptions: {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
