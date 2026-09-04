7ff367ec6313b6b53b58579b277e3110d7c77b92
Gregor Arz <upstroke@webtutorials.de>
2026-09-04 16:47:19 +0200

    test: remove obsolete example.spec.js

--- tests/acceptance/example.spec.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
-
-test('home page has correct title', async ({ page }) => {
-	await page.goto('/');
-	await expect(page).toHaveTitle(/Home TMDB/);
-});
