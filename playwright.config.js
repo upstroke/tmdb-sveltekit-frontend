3ed4a00605f5b76564039a183691493803a8630f
Gregor Arz <upstroke@webtutorials.de>
2026-09-04 15:21:47 +0200

    chore: add browser projects to playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -1,20 +1,22 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	testDir: 'tests/acceptance',
-	use: {
-		baseURL: 'http://127.0.0.1:4173',
-		trace: 'on-first-retry'
-	},
-	webServer: {
-		command: 'npm run build && npm run preview -- --host 127.0.0.1 --port 4173',
-		url: 'http://127.0.0.1:4173',
-		reuseExistingServer: !process.env.CI
-	},
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] }
-		}
-	]
+  testDir: './tests/acceptance',
+  timeout: 30 * 1000,
+  fullyParallel: true,
+  use: {
+    baseURL: 'http://127.0.0.1:4173',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        browserName: 'chromium',
+        channel: 'chrome',
+      },
+    },
+  ],
+  reporter: 'list',
 });
