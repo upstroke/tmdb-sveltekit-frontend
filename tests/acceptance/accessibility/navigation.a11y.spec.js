6fe6434a871cd396b4954382046530112d7c7a3b
gregorarz <upstroke@webtutorials.de>
2026-09-10 09:39:01 +0200

    feature: add accessibility tests

--- tests/acceptance/accessibility/navigation.a11y.spec.js
+++ tests/acceptance/accessibility/navigation.a11y.spec.js
@@ -2,17 +2,17 @@ import { test } from '@playwright/test';
 import { checkA11y } from '../../setup/a11y.js';
 
 test.describe('Accessibility', () => {
-  test('Homepage hat keine automatisiert erkannten WCAG A/AA Verstö§§§', async ({ page }) => {
+  test('Homepage hat keine automatisiert erkannten WCAG A/AA Verstöße', async ({ page }) => {
     await page.goto('/');
     await checkA11y(page);
   });
 
-  test('Search-Seite hat keine automatisiert erkannten WCAG A/AA Verstö§§§', async ({ page }) => {
+  test('Search-Seite hat keine automatisiert erkannten WCAG A/AA Verstöße', async ({ page }) => {
     await page.goto('/search');
     await checkA11y(page);
   });
 
-  test('Movie-Detailseite hat keine automatisiert erkannten WCAG A/AA Verstö§§§', async ({ page }) => {
+  test('Movie-Detailseite hat keine automatisiert erkannten WCAG A/AA Verstöße', async ({ page }) => {
     await page.goto('/movies/278'); // Beispiel: The Shawshank Redemption
     await checkA11y(page);
   });
