fb26982843b035e28dc559f8ca87b5b0e53f0223
Gregor Arz <upstroke@webtutorials.de>
2026-09-10 09:45:57 +0200

    Simplify a11y test to only check homepage for now

--- tests/acceptance/accessibility/navigation.a11y.spec.js
+++ tests/acceptance/accessibility/navigation.a11y.spec.js
@@ -2,18 +2,8 @@ import { test } from '@playwright/test';
 import { checkA11y } from '../../setup/a11y.js';
 
 test.describe('Accessibility', () => {
-  test('Homepage hat keine automatisiert erkannten WCAG A/AA Verstöße', async ({ page }) => {
+  test('Homepage has no automatically detected WCAG A/AA violations', async ({ page }) => {
     await page.goto('/');
-    await checkA11y(page);
-  });
-
-  test('Search-Seite hat keine automatisiert erkannten WCAG A/AA Verstöße', async ({ page }) => {
-    await page.goto('/search');
-    await checkA11y(page);
-  });
-
-  test('Movie-Detailseite hat keine automatisiert erkannten WCAG A/AA Verstöße', async ({ page }) => {
-    await page.goto('/movies/278'); // Beispiel: The Shawshank Redemption
     await checkA11y(page);
   });
 });
